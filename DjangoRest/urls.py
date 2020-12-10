563215e1415bf74c491da7ee1e14246466637c76
Irina Nikolova <38880477+Irinkaaa@users.noreply.github.com>
2020-12-10 17:46:29 +0200

    Completed

--- DjangoRest/urls.py
+++ DjangoRest/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('api/', include('books_api.urls')),
+    # path('api/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
 ]
--- books_api/urls.py
+++ books_api/urls.py
@@ -1,3 +1,7 @@
-urlpatterns = [
+from django.urls import path
+from books_api.views import ListBookView, DetailBookView
 
+urlpatterns = [
+    path('books/', ListBookView.as_view(), name='books list api'),
+    path('books/<int:pk>/', DetailBookView.as_view(), name='book details api'),
 ]
