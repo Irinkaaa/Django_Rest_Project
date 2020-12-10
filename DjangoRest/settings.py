7ab645074fd932763d1ee484216776a5dd8194e8
Irina Nikolova <38880477+Irinkaaa@users.noreply.github.com>
2020-12-10 15:32:42 +0200

    Initial Commit

--- DjangoRest/settings.py
+++ DjangoRest/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'books_api',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'DjangoRest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'books_db',
+        'USER': 'postgres',
+        'PASSWORD': '12345qwe',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
